8e2c848a55f24e3136fbba33fc6956b9eb550af1
Sergei2580362 <s2580362@gmail.com>
2023-06-19 01:35:25 +0300

    мелкие изменения

--- PIZZA/src/make-order/index.js
+++ PIZZA/src/make-order/index.js
@@ -1,10 +1,7 @@
 const button = document.createElement('button');
 button.textContent = 'ЗАКАЗАТЬ';
-
 button.addEventListener('click', () => {
   alert('ЗАКАЗ ОФОРМЛЕН!');
 });
-
 const div = document.querySelector('.make-order');
-
 div.appendChild(button);
