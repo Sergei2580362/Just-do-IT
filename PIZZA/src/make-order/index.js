c3ab7b86f840327c081b6f1f228d3b069780c6c1
unknown <miledy18@gmail.com>
2023-06-23 16:17:06 +0200

    menu

--- PIZZA/src/make-order/index.js
+++
@@ -1,7 +0,0 @@
-const button = document.createElement('button');
-button.textContent = 'ЗАКАЗАТЬ';
-button.addEventListener('click', () => {
-  alert('ЗАКАЗ ОФОРМЛЕН!');
-});
-const div = document.querySelector('.make-order');
-div.appendChild(button);
